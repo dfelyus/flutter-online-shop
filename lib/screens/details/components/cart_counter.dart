be263b7f9c35e22ecae455e931734d1c6b31c096
elyus77 <dfelyus@gmail.com>
2022-07-09 16:45:43 +0200

    first commit

---
+++ lib/screens/details/components/cart_counter.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class CartCounter extends StatefulWidget {
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        buildOutlineButton(
+          Icons.remove,
+          () {
+            if (numOfItems > 1) {
+              setState(() {
+                numOfItems--;
+              });
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin / 2),
+          child: Text(
+            // if our item is less  then 10 then  it shows 01 02 like that
+            numOfItems.toString().padLeft(2, "0"),
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        buildOutlineButton(Icons.add, () {
+          setState(() {
+            numOfItems++;
+          });
+        }),
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton(IconData icon, Function press) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlineButton(
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(13),
+        ),
+        onPressed: () => press(),
+        child: Icon(icon),
+      ),
+    );
+  }
+}
